eb66e26e705005edfde2d04536af21f20b1c999c
luj <david_lu_st@163.com>
2019-04-15 19:28:05 +0800

    增加协议处理器接口

--- jamver/src/main/java/luj/game/server/api/net/GameProtoHandler.java
+++ jamver/src/main/java/luj/game/server/api/net/GameProtoHandler.java
@@ -2,8 +2,35 @@ package luj.game.server.api.net;
 
 public interface GameProtoHandler<P> {
 
+  @interface Register {
+
+    String value();
+  }
+
   interface Context {
 
+    <P> P proto(GameProtoHandler<P> handler);
+
+    Player player();
+
+    Service service();
+  }
+
+  interface Player {
+
+    Long getPlayerId();
+
+    void sendProto();
+  }
+
+  interface Service {
+
+    Config config(Class<?> configType);
+  }
+
+  interface Config {
+
+    <C> C find(Comparable<?> id);
   }
 
   void onHandle(Context ctx);
