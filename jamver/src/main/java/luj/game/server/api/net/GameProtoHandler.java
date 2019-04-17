8ed54528c2e310830ca2c88153feaa48d1e21e17
luj <david_lu_st@163.com>
2019-04-17 10:19:59 +0800

    完善注册注解声明

--- jamver/src/main/java/luj/game/server/api/net/GameProtoHandler.java
+++ jamver/src/main/java/luj/game/server/api/net/GameProtoHandler.java
@@ -1,7 +1,18 @@
 package luj.game.server.api.net;
 
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import org.springframework.stereotype.Component;
+
 public interface GameProtoHandler<P> {
 
+  @Target(ElementType.TYPE)
+  @Retention(RetentionPolicy.RUNTIME)
+  @Documented
+  @Component
   @interface Register {
 
     String value();
