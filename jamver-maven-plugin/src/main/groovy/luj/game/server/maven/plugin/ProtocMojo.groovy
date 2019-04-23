02e35fd4625d7d4ef936eb53af36e77624d29fad
luj <david_lu_st@163.com>
2019-04-23 13:56:40 +0800

    添加maven插件入口类

--- jamver-maven-plugin/src/main/groovy/luj/game/server/maven/plugin/ProtocMojo.groovy
+++ jamver-maven-plugin/src/main/groovy/luj/game/server/maven/plugin/ProtocMojo.groovy
@@ -1,24 +1,24 @@
 package luj.game.server.maven.plugin
 
+import groovy.transform.CompileStatic
+import luj.game.server.api.net.NetProto
+import luj.proto.maven.plugin.generate.ProtoAllGenerator
 import org.apache.maven.plugin.AbstractMojo
-import org.apache.maven.plugin.MojoExecutionException
-import org.apache.maven.plugin.MojoFailureException
 import org.apache.maven.plugins.annotations.LifecyclePhase
 import org.apache.maven.plugins.annotations.Mojo
+import org.apache.maven.plugins.annotations.Parameter
+import org.apache.maven.project.MavenProject
 
+@CompileStatic
 @Mojo(name = 'protoc', defaultPhase = LifecyclePhase.GENERATE_SOURCES)
 class ProtocMojo extends AbstractMojo {
 
-//  @Parameter(defaultValue = '${project}', readonly = true)
-//  private MavenProject _project
-
   @Override
-  void execute() throws MojoExecutionException, MojoFailureException {
-    println('测测测测测测测测测测测测测测测测测测测测测测测测测测测测测测试')
-    println('测测测测测测测测测测测测测测测测测测测测测测测测测测测测测测试')
-    println('测测测测测测测测测测测测测测测测测测测测测测测测测测测测测测试')
-    println('测测测测测测测测测测测测测测测测测测测测测测测测测测测测测测试')
-    println('测测测测测测测测测测测测测测测测测测测测测测测测测测测测测测试')
-    println('测测测测测测测测测测测测测测测测测测测测测测测测测测测测测测试')
+  void execute() {
+    ProtoAllGenerator generator = ProtoAllGenerator.Factory.create(_project, NetProto, getLog())
+    generator.generate()
   }
+
+  @Parameter(defaultValue = '${project}', readonly = true)
+  private MavenProject _project
 }
