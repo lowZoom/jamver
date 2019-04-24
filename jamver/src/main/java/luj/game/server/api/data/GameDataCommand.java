9b164040399a60cce337a1259c2c3a760028d869
luj <david_lu_st@163.com>
2019-04-24 18:06:13 +0800

    修正CMD注解声明

--- jamver/src/main/java/luj/game/server/api/data/GameDataCommand.java
+++ jamver/src/main/java/luj/game/server/api/data/GameDataCommand.java
@@ -1,9 +1,20 @@
 package luj.game.server.api.data;
 
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import org.springframework.stereotype.Component;
+
 public interface GameDataCommand<P, D> {
 
+  @Target(ElementType.TYPE)
+  @Retention(RetentionPolicy.RUNTIME)
+  @Documented
+  @Component
   @interface Register {
-
+    // NOOP
   }
 
   interface Context {
