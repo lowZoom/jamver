0abe7c5919523cd03c1c4c55bd86305ba1ebe61a
luj <david_lu_st@163.com>
2019-05-05 12:05:41 +0800

    调整数据载入接口

--- jamver/src/main/java/luj/game/server/api/data/GameDataLoad.java
+++ jamver/src/main/java/luj/game/server/api/data/GameDataLoad.java
@@ -8,7 +8,10 @@ public interface GameDataLoad<P, R> {
 
     <P> P param(GameDataLoad<P, ?> load);
 
+    @Deprecated
     <R, F> AndLoad<R, F> load(Function<R, F> field, Comparable<?> id);
+
+    <R, F> AndLoad<R, F> load(Function<R, F> field, Class<F> dataType, Comparable<?> dataId);
   }
 
   interface AndLoad<R, F> {
