c02cb45576d214795dae9db4f2307f3e9f73ec1a
luj <david_lu_st@163.com>
2019-05-14 09:40:28 +0800

    缓存请求增加动态参数

--- jamver/src/main/java/luj/game/server/internal/data/load/field/LoadFieldMeta.java
+++ jamver/src/main/java/luj/game/server/internal/data/load/field/LoadFieldMeta.java
@@ -2,19 +2,11 @@ package luj.game.server.internal.data.load.field;
 
 import java.util.function.BiConsumer;
 
-public class LoadField<R, F> {
+public class LoadFieldMeta<R, F> {
 
-  public LoadField(Class<F> dataType, BiConsumer<R, F> fieldSetter) {
+  public LoadFieldMeta(Class<F> dataType, BiConsumer<R, F> fieldSetter) {
     _dataType = dataType;
     _fieldSetter = fieldSetter;
-  }
-
-  public void setValue(F value) {
-    _value = value;
-  }
-
-  public F getValue() {
-    return _value;
   }
 
   public Class<F> getDataType() {
@@ -24,8 +16,6 @@ public class LoadField<R, F> {
   public BiConsumer<R, F> getFieldSetter() {
     return _fieldSetter;
   }
-
-  private F _value;
 
   private final Class<F> _dataType;
 
