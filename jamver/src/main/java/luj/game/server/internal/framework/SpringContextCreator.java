f88ece07ce40c26a96042b489c9ddd00a8821a95
luj <david_lu_st@163.com>
2020-05-29 09:27:58 +0800

    精简spring上下文创建

--- jamver/src/main/java/luj/game/server/internal/framework/SpringContextCreator.java
+++ jamver/src/main/java/luj/game/server/internal/framework/SpringContextCreator.java
@@ -1,16 +1,12 @@
 package luj.game.server.internal.framework;
 
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigUtils;
 
 public class SpringContextCreator {
 
   public AnnotationConfigApplicationContext create() {
     AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext();
-
-    ctx.getBeanFactory().registerSingleton(
-        AnnotationConfigUtils.CONFIGURATION_BEAN_NAME_GENERATOR, new BeanNameGen());
-
+    ctx.setBeanNameGenerator(new BeanNameGen());
     return ctx;
   }
 }
